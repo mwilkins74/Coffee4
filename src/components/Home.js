0d2bccab0b0bb80fe380afcc8e80c108fa35fea3
Mark <markwilkins86@gmail.com>
2022-06-22 15:22:43 -0400

    Delete Function, Update Function. Yay CRUD.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import CoffeeCard from "./CoffeeCard"
+import React from "react";
+import CoffeeCard from "./CoffeeCard";
 
-function Home({ coffees, setCoffees }) {
-    const coffeeCards = coffees.map((coffee) => (
-      <CoffeeCard
-        key={coffee.id}
-        id={coffee.id}
-        name={coffee.name}
-        origin={coffee.origin_country}
-        roaster={coffee.roaster}
-        price={coffee.price}
-        image={coffee.image_url}
-        stock={coffee.stock}
-        setCoffees={setCoffees}
-      />
-    ));
+function Home({ coffees, setCoffees, setDelete, isDeleted }) {
+  const coffeeCards = coffees.map((coffee) => (
+    <CoffeeCard
+      key={coffee.id}
+      id={coffee.id}
+      name={coffee.name}
+      origin={coffee.origin_country}
+      roaster={coffee.roaster}
+      price={coffee.price}
+      image={coffee.image_url}
+      stock={coffee.stock}
+      setCoffees={setCoffees}
+      setDelete={setDelete}
+      isDeleted={isDeleted}
+    />
+  ));
 
-  return (
-      <div id="coffee-collection">{coffeeCards}</div>
-      
-  )
+  return <div id="coffee-collection">{coffeeCards}</div>;
 }
 
-export default Home
+export default Home;
